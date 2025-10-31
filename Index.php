5e0beaa8447dec84be85db7812211c4867adfe1d
R3dpriest <redisia@yahoo.com>
2025-10-31 23:06:39 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,4 +4,4 @@ include_once __dir__ . DIRECTORY_SEPARATOR . "CMS_Template_Filestart.php";
 
 if(session_status() !== PHP_SESSION_ACTIVE){session_start();}
 ?>
-<br><br>Hello world
+<br><br>Hello world<br>
